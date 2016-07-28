e9edf7b4f56d086d379470b80a7bd578d4d716aa
Marcos Adriel <marcosadriel@gmail.com>
2016-07-28 15:55:54 -0400

    Atividade 1016

---
+++ urijudge_basictasks/Task1016.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urijudge_basictasks;
+
+/**
+ *
+ * @author marcos.rost
+ */
+public class Task1016 {
+    
+}
